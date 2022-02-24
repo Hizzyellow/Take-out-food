e28539b4d61cdaf57e1f2ea4ffef62b62302c80d
Hizzyellow <1329377189@qq.com>
2022-02-24 16:04:39 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,12 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评价</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item"><router-link to="/goods">商品</router-link></div>
+      <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
+      <div class="tab-item"><router-link  to="/seller">商家</router-link> </div>
     </div>
-    <div class="content">
-      I am content
-    </div>
+    <router-view></router-view>
   </div>
-
 </template>
 
 <script>
@@ -32,4 +29,10 @@
       .tab-item
         flex : 1
         text-align :center
+        &>a
+          display :block
+          font-size :0.875rem
+          color :rgb(77,85,93)
+          &.active
+            color :rgb(240,20,20)
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import goods from 'components/goods/goods.vue';
+import seller from 'components/seller/seller.vue';
+import ratings from 'components/ratings/ratings.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/goods'
+    },
+    {
+      path: '/goods',
+      component: goods
+    },
+    {
+      path: '/seller',
+      component: seller
+    },
+    {
+      path: '/ratings',
+      component: ratings
     }
   ]
 });
